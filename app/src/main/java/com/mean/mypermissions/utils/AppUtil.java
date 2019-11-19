174dfff55667d3c842348f4c7268aeece1e79167
MeanFan <meanfan@outlook.com>
2019-11-19 16:23:45 +0800

    add getAppName by packageName

--- app/src/main/java/com/mean/mypermissions/utils/AppUtil.java
+++ app/src/main/java/com/mean/mypermissions/utils/AppUtil.java
@@ -7,15 +7,20 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.mean.mypermissions.R;
 import com.mean.mypermissions.bean.AppConfig;
 import com.mean.mypermissions.bean.PermissionConfigs;
+import com.mean.mypermissions.bean.RestrictMode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class PermissionUtil {
-    public static final String TAG = "PermissionUtil";
+public class AppUtil {
+    public static final String TAG = "AppUtil";
     public static List<PackageInfo> getAllAppInfo(Context context) {
         PackageManager pm = context.getPackageManager();
         List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.GET_PERMISSIONS);
@@ -37,9 +42,22 @@ public class PermissionUtil {
          return pm.getApplicationLabel(info).toString();
     }
 
+    public static String getAppName(Context context, String pkg) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo(pkg,0);
+            return getAppName(context, applicationInfo);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
     public static List<AppConfig> getAllUserAppConfigs(Context context) {
         List<PackageInfo> packageInfos = getAllAppInfo(context);
-        List<AppConfig> appConfigs = new ArrayList<>();
+        List<AppConfig> appConfigs = Collections.synchronizedList(new ArrayList<AppConfig>());
         for(PackageInfo packageInfo:packageInfos){
             //TODO 跳过系统应用
 //            if(packageInfo.applicationInfo.flags == ApplicationInfo.FLAG_SYSTEM ||
@@ -71,5 +89,24 @@ public class PermissionUtil {
             appConfig.setPermissionConfigs(permissionConfigs);
         }
         return  true;
+    }
+
+    public static String getRestrictModeName(Context context, RestrictMode mode){
+        switch (mode){
+            case ALLOW:
+                return context.getString(R.string.restrict_mode_allow);
+            case DENY:
+                return context.getString(R.string.restrict_mode_deny);
+            case DEFAULT:
+                return context.getString(R.string.restrict_mode_default);
+            case REQUEST:
+                return context.getString(R.string.restrict_mode_request);
+            case ALLOW_BUT_FAKE:
+                return context.getString(R.string.restrict_mode_allow_but_fake);
+            case ALLOW_BUT_NULL:
+                return context.getString(R.string.restrict_mode_allow_but_null);
+            default:
+                return null;
+        }
     }
 }
