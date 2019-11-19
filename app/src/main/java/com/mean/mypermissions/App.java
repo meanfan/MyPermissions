aaa82ac787203df691cd9c99bc6f7a83d78e6a68
MeanFan <meanfan@outlook.com>
2019-11-19 16:23:05 +0800

    add Application

---
+++ app/src/main/java/com/mean/mypermissions/App.java
@@ -0,0 +1,11 @@
+package com.mean.mypermissions;
+
+import android.app.Application;
+
+import com.mean.mypermissions.bean.AppConfig;
+
+import java.util.List;
+
+public class App extends Application {
+    public static List<AppConfig> appConfigs;
+}
